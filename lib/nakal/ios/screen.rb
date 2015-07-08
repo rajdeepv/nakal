e3ebb7d7b9bae0fdd66b544c2d8120521c893d56
Rajdeep <mail.rajvarma@gmail.com>
2015-07-08 20:03:39 +0530

    refactoring

--- lib/nakal/ios/screen.rb
+++ lib/nakal/ios/screen.rb
@@ -17,8 +17,7 @@ module Nakal
         sleep 1
         Dir.glob(File.expand_path('~/Desktop/iOS\\ Simulator\\ Screen\\ Shot - Apple\\ Watch*')).each{|f| FileUtils.rm(f)}
         latest_file = Dir.glob(File.expand_path('~/Desktop/iOS\\ Simulator\\ Screen\\ Shot*')).max_by { |f| File.mtime(f) }
-        `mv  #{Shellwords.shellescape(latest_file)} #{Nakal.image_location}`
-        `mv #{Nakal.image_location}/iOS\\ Simulator\\ Screen\\ Shot* #{Nakal.image_location}/#{@name}.png`
+        File.rename(latest_file,"#{Nakal.image_location}/#{@name}.png")
       end
 
     end
