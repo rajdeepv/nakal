ec3b3f24ec0ae164e2695590954acc6a5c8db663
Rajdeep Verma <mail.rajvarma@gmail.com>
2016-02-17 12:20:47 +0530

    fixing!

--- lib/nakal/ios/screen.rb
+++ lib/nakal/ios/screen.rb
@@ -5,8 +5,6 @@ module Nakal
       private
 
       def capture_script
-        xcode_7 = !(%x(instruments -s devices ; echo)).match(' Simulator\)')
-        simulator_app_name = xcode_7 ? "Simulator" : "iOS Simulator"
         "osascript <<EOF
         tell application \"#{simulator_app_name}\"
         activate
@@ -18,12 +16,16 @@ module Nakal
         EOF"
       end
 
+      def simulator_app_name
+        xcode_7 = !(%x(instruments -s devices ; echo)).match(' Simulator\)')
+        xcode_7 ? "Simulator" : "iOS Simulator"
+      end
 
       def capture
         `#{capture_script}`
         sleep 1
-        Dir.glob(File.expand_path('~/Desktop/iOS\\ Simulator\\ Screen\\ Shot - Apple\\ Watch*')).each { |f| FileUtils.rm(f) }
-        latest_file = Dir.glob(File.expand_path('~/Desktop/iOS\\ Simulator\\ Screen\\ Shot*')).max_by { |f| File.mtime(f) }
+        Dir.glob(File.expand_path("~/Desktop/#{simulator_app_name}\\ Screen\\ Shot - Apple\\ Watch*")).each { |f| FileUtils.rm(f) }
+        latest_file = Dir.glob(File.expand_path("~/Desktop/#{simulator_app_name}\\ Screen\\ Shot*")).max_by { |f| File.mtime(f) }
         File.rename(latest_file, "#{Nakal.image_location}/#{@name}.png")
       end
 
